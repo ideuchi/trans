ed30a976b75fa216bf7c8c8f5dc7bf43083496cb
Ideuchi Masao <mideuchi@mastar.nict.go.jp>
2021-10-05 16:32:58 +0900

    added getting information from arxiv

--- trans_api/views.py
+++ trans_api/views.py
@@ -130,7 +130,7 @@ def arxiv_check(request):
         paper_info += 'Published Date: '+result.published.strftime('%Y/%m/%d %H:%M:%S')+'\n'
         post_res = CLIENT.api_call(api_method='chat.postMessage', json={'channel': post_channel, 'text': paper_info})
         debug_msg('arXiv english information post_res: '+str(post_res))
-        CLIENT.api_call(api_method='chat.postMessage', json={'channel': post_channel, 'text': paper_info_rep, 'thread_ts': post_res['ts'] }
+        CLIENT.api_call(api_method='chat.postMessage', json={'channel': post_channel, 'text': paper_info_rep, 'thread_ts': post_res['ts'] })
         message += '-----\npaper_info_en: \n'+paper_info+'\npaper_info_'+trans_tgt_lang+': '+paper_info_rep+'\n'
     debug_msg('/arxiv_check result:\n' + message)
     return HttpResponse('')
